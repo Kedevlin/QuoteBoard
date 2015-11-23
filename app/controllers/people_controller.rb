b75139e96b5aface19f5117f17821809b8b7830e
Dreedle <davis.audrey.lynn@gmail.com>
2015-11-23 15:44:21 -0800

    Dropdown menu leads to a page with details about person

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,28 +1,23 @@
 class PeopleController < ApplicationController
-  before_action :get_people, :get_person
+  before_action :get_people
 
-  def home
+  def get_people
+    @people = Person.all
   end
 
-  def select
-    redirect_to person_home_path(params[:person][:id])
+  def home
   end
 
   def person_home
     @person = Person.find(params[:id])
   end
 
-  def get_people
-    @people = Person.all
+  def select
+    redirect_to person_home_path(params[:person][:id])
   end
 
-  def get_person
-    @person = Person.find(params[:id])
-  end
 
-  def redirect_to_person
-    render :show
-  end
+
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
   get 'home/' => 'home#index'
+  get 'people/select' => 'people#select', as: :person_select
+  get 'people/:id/home' => 'people#person_home', as: :person_home
 
   resources :people do
     resources :spoken_quotes
