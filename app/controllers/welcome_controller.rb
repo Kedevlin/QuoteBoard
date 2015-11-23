4756256b898cfe815f25540f1e203b8dc0101680
Dreedle <davis.audrey.lynn@gmail.com>
2015-11-23 11:31:27 -0800

    Welcome page shows a random quote

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
 class WelcomeController < ApplicationController
+  before_action :get_quotes
+
+  def get_quotes
+    @quotes = Quote.all
+  end
+
   def index
+    @random_quote = @quotes[rand(@quotes.count)].body
   end
 end
