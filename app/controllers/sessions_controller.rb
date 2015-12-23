4032d568e21a80bc1bfafaca1a045e4eb721f1fd
Kelly <kelly_devlin@alumni.brown.edu>
2015-12-22 20:22:21 -0800

    complete create and destroy methods in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,22 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    data = params[:session_data]
+    @person = Person.find_by_username(data[:username])
+    if !@person.nil? && @person.authenticate(data[:password])
+      session[:person_id] = @person.id
+      flash[:success] = "You are now logged in."
+      redirect_to root_path
+    else
+      flash.now[:error] = "Invalid username and password combination."
+      render :new
+    end
   end
 
   def destroy
-
+    @current_user = nil
+    session[:person_id] = nil
+    flash[:success] = "You are now logged out."
+    redirect_to root_path
   end
 end
