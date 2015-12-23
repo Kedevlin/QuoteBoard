d6fb5cde3e64d6111aa00c59e42d05a381c47162
Kelly <kelly_devlin@alumni.brown.edu>
2015-12-22 18:12:02 -0800

    Update Person model with has_secure_password and column validations

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,7 @@
 class Person < ActiveRecord::Base
+  has_secure_password
+  validates_uniqueness_of :username
+  validates :name, :username, presence: true
   has_many :spoken_quotes, :class_name => "Quote", :foreign_key => :speaker_id,:dependent => :destroy
   has_many :posted_quotes, :class_name => "Quote", :foreign_key => :poster_id, :dependent => :destroy
   has_many :comments, :class_name => "Comment", :foreign_key => :commenter_id, :dependent => :destroy
