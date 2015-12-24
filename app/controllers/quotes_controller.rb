5e4a81dec2331ee4d214bd32135354cee40f4546
Kelly <kelly_devlin@alumni.brown.edu>
2015-12-24 14:45:25 -0500

    complete quote#create

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -4,8 +4,8 @@ class QuotesController < ApplicationController
     Quote.create(
     body: quote_params[:quote][:body],
     speaker_id: quote_params[:quote][:speaker_id],
-    poster_id: params[:id])
-    redirect_to person_home_path(params[:id])
+    poster_id: current_person.id)
+    redirect_to person_home_path(current_person)
   end
 
   def destroy
@@ -30,6 +30,7 @@ class QuotesController < ApplicationController
 
   def new
     @quote = Quote.new
+    @people_options = Person.all.map{|p| [ p.name, p.id ] }
   end
 
   def show
