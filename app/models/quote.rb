5f48f130e0cf0c5d54973b4f6f0a0f6421bfb9cd
Kelly <kelly_devlin@alumni.brown.edu>
2015-12-25 23:14:08 -0500

    Require name and body on all quotes

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,5 @@ class Quote < ActiveRecord::Base
   belongs_to :speaker, :class_name => "Person", :foreign_key => :speaker_id
   belongs_to :poster, :class_name => "Person", :foreign_key => :poster_id
   has_many :comments, :dependent => :destroy
+  validates :speaker_id, :body, :poster_id, presence: true
 end
